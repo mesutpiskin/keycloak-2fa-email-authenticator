1ba0fd684238b0721a8c62093de5661b15d7f34e
Mesut Pişkin <mesutpiskin@outlook.com>
2022-10-20 22:20:21 +0300

    Custom provider for sending otp via email

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# keycloak-2fa-email-authenticator-
-Keycloak Authentication Provider implementation to get a 2nd-factor authentication with a OTP/code/token send via Email (through SMTP)
+# Keycloak 2FA Email Authenticator
+
+Keycloak Authentication Provider implementation to get a 2nd-factor authentication with an OTP/code/token send via Email (through SMTP)
+
+When logging in with this provider, you can send a verification code (otp) to the user's e-mail address.
+Tested with Keycloak version 19.0.3, if you are using different Keycloak version, don't forget to change the version in pom.xml file.
+
+The [Server Development part of the Keycloak reference documentation](https://www.keycloak.org/docs/latest/server_development/index.html) contains additional resources and examples for developing custom Keycloak extensions.
+
+# Deployment
+
+## Provider
+
+`mvn package` will be create a jar file.
+copy _keycloak-2fa-email-authenticator-1.0.0.0-SNAPSHOT.jar_ to _keycloak/providers/_ directory.
+
+if you are Dockerized keycloak then copy to _/opt/jboss/keycloak/standalone/deployments/_ directory.
+
+## Theme Resources
+
+**html/code-email.ftl** is a html email template. Copy to _themes/base/email/html/_
+
+**text/code-email.ftl**  Copy to _themes/base/email/text/_
+
+**messages/*.properties** Append to _themes/base/email/messages/messages_en.properties_
+
+# Configuration
+
+## Email Configuration
+SMTP setting configure for e-mail send.
+_Realm Settings/Email_
+
+## Authentication Flow
+Create new browser login authentication flow and add Email OTP flow before Username Password Form.
+
+<img src="static/otp-form.png">
---
+++ src/main/java/com/mesutpiskin/keycloak/auth/email/EmailAuthenticatorFormFactory.java
@@ -0,0 +1,80 @@
+package com.mesutpiskin.keycloak.auth.email;
+
+import com.google.auto.service.AutoService;
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+@AutoService(AuthenticatorFactory.class)
+public class EmailAuthenticatorFormFactory implements AuthenticatorFactory {
+
+    @Override
+    public String getDisplayType() {
+        return "Email OTP";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    public static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED, AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Email otp authenticator.";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+        // NOOP
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return new EmailAuthenticatorForm(session);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        // NOOP
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+        // NOOP
+    }
+
+    @Override
+    public String getId() {
+        return EmailAuthenticatorForm.ID;
+    }
+}
