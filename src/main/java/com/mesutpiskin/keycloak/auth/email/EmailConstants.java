b5e689cf2e558ff6bd2b49224c212fc5471af79c
Mathieu Passenaud <mathieu.passenaud@please-open.it>
2025-06-01 10:37:29 +0200

    removed lombok, update java 21

--- src/main/java/com/mesutpiskin/keycloak/auth/email/EmailConstants.java
+++ src/main/java/com/mesutpiskin/keycloak/auth/email/EmailConstants.java
@@ -1,12 +1,9 @@
 package com.mesutpiskin.keycloak.auth.email;
 
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
 public class EmailConstants {
-	public String CODE = "emailCode";
-	public String CODE_LENGTH = "length";
-	public String CODE_TTL = "ttl";
-	public int DEFAULT_LENGTH = 6;
-	public int DEFAULT_TTL = 300;
+	public static String CODE = "emailCode";
+	public static String CODE_LENGTH = "length";
+	public static String CODE_TTL = "ttl";
+	public static int DEFAULT_LENGTH = 6;
+	public static int DEFAULT_TTL = 300;
 }
