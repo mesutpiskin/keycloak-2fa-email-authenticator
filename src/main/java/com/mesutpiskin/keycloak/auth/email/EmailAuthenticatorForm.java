1664a6d34948fdacccfe1fd480050c988d42c0a9
Mustafa Kamel <mustafa.kamel@gotocme.com>
2025-11-25 03:47:27 +0200

    Add override annotation to the disabledByBruteForceError method

--- src/main/java/com/mesutpiskin/keycloak/auth/email/EmailAuthenticatorForm.java
+++ src/main/java/com/mesutpiskin/keycloak/auth/email/EmailAuthenticatorForm.java
@@ -210,6 +210,7 @@ public class EmailAuthenticatorForm extends AbstractUsernameFormAuthenticator
         }
     }
 
+    @Override
     protected String disabledByBruteForceError() {
         return Messages.INVALID_ACCESS_CODE;
     }
